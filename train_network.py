765f28d6c4d97421e49a6eea174a4af64f74ba7c
Shalabh Singh <shalabhsingh95@gmail.com>
2017-08-27 01:00:57 +0530

    Major bug update
    This update includes correction of incorrect state storage in the episode_state matrix. Also fps is increased for faster training

--- train_network.py
+++ train_network.py
@@ -17,19 +17,22 @@ import tensorflow as tf
 import pygame
 import wrapped_flappy_bird as game
 
+import scipy.misc
+import scipy.stats as st
+
 import threading
 import time
 import math
 
 GAMMA = 0.99                #discount value
 BETA = 0.01                 #regularisation coefficient
-IMAGE_ROWS = 84
+IMAGE_ROWS = 85
 IMAGE_COLS = 84
 IMAGE_CHANNELS = 4
-LEARNING_RATE = 7e-4	
+LEARNING_RATE = 7e-4
 EPISODE = 0
 THREADS = 16
-t_max = 5
+t_max = 5  
 const = 1e-5
 T = 0
 
@@ -39,18 +42,20 @@ episode_output = []
 episode_critic = []
 
 ACTIONS = 2
-FIRST_FRAME = True 
 a_t = np.zeros(ACTIONS)
 
-def binarycrossentropy(y_true, y_pred):     #policy loss
+#loss function for policy output
+def logloss(y_true, y_pred):     #policy loss
 	return -K.sum( K.log(y_true*y_pred + (1-y_true)*(1-y_pred) + const), axis=-1) 
 	# BETA * K.sum(y_pred * K.log(y_pred + const) + (1-y_pred) * K.log(1-y_pred + const))   #regularisation term
 
+#loss function for critic output
 def sumofsquares(y_true, y_pred):        #critic loss
 	return K.sum(K.square(y_pred - y_true), axis=-1)
 
+#function buildmodel() to define the structure of the neural network in use 
 def buildmodel():
-	print("Model buliding begins")
+	print("Model building begins")
 
 	model = Sequential()
 	keras.initializers.RandomUniform(minval=-0.1, maxval=0.1, seed=None)
@@ -65,19 +70,20 @@ def buildmodel():
 
 	model = Model(inputs = S, outputs = [P,V])
 	rms = RMSprop(lr = LEARNING_RATE, rho = 0.99, epsilon = 0.1)
-	model.compile(loss = {'o_P': binarycrossentropy, 'o_V': sumofsquares}, loss_weights = {'o_P': 1., 'o_V' : 0.5}, optimizer = rms)
+	model.compile(loss = {'o_P': logloss, 'o_V': sumofsquares}, loss_weights = {'o_P': 1., 'o_V' : 0.5}, optimizer = rms)
 	return model
 
+#function to preprocess an image before giving as input to the neural network
 def preprocess(image):
 	image = skimage.color.rgb2gray(image)
-	image = skimage.transform.resize(image, (IMAGE_ROWS,IMAGE_COLS), mode = 'constant')	
+	image = skimage.transform.resize(image, (IMAGE_ROWS, IMAGE_COLS), mode = 'constant')	
 	image = skimage.exposure.rescale_intensity(image, out_range=(0,255))
 	image = image.reshape(1, image.shape[0], image.shape[1], 1)
 	return image
 
-
+# initialize a new model using buildmodel() or use load_model to resume training an already trained model
 model = buildmodel()
-#model = load_model("saved_models/model_updates", custom_objects={'binarycrossentropy': binarycrossentropy, 'sumofsquares': sumofsquares})
+#model = load_model("saved_models/model_updates3900", custom_objects={'logloss': logloss, 'sumofsquares': sumofsquares})
 model._make_predict_function()
 graph = tf.get_default_graph()
 
@@ -89,14 +95,15 @@ a_t[0] = 1 #index 0 = no flap, 1= flap
 
 game_state = []
 for i in range(0,THREADS):
-	game_state.append(game.GameState())
+	game_state.append(game.GameState(30000))
 
 
-def runprocess(thread_id, s_t, FIRST_FRAME, t):
+def runprocess(thread_id, s_t):
 	global T
 	global a_t
 	global model
 
+	t = 0
 	t_start = t
 	terminal = False
 	r_t = 0
@@ -106,68 +113,57 @@ def runprocess(thread_id, s_t, FIRST_FRAME, t):
 	critic_store = []
 	s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])
 
-	if FIRST_FRAME:
-		a_t = [1,0]
-
 	while t-t_start < t_max and terminal == False:
 		t += 1
 		T += 1
-		intermediate_output = 0
-		
-		if FIRST_FRAME == False:
-			with graph.as_default():
-				out = model.predict(s_t)[0]			
-				intermediate_output = intermediate_layer_model.predict(s_t)
-			no = np.random.rand()
-			a_t = [0,1] if no < out else [1,0]  #stochastic action
-			#a_t = [0,1] if 0.5 <y[0] else [1,0]  #deterministic action
+		intermediate_output = 0	
+
+		with graph.as_default():
+			out = model.predict(s_t)[0]			
+			intermediate_output = intermediate_layer_model.predict(s_t)
+		no = np.random.rand()
+		a_t = [0,1] if no < out else [1,0]  #stochastic action
+		#a_t = [0,1] if 0.5 <y[0] else [1,0]  #deterministic action
 
 		x_t, r_t, terminal = game_state[thread_id].frame_step(a_t)
 		x_t = preprocess(x_t)
 
-		if FIRST_FRAME:
-			s_t = np.concatenate((x_t, x_t, x_t, x_t), axis=3)
-			FIRST_FRAME = False
-		else:
-			s_t = np.append(x_t, s_t[:, :, :, :3], axis=3)
-
-		y = 0 if a_t[0] == 1 else 1
-		
 		with graph.as_default():
 			critic_reward = model.predict(s_t)[1]
+
+		y = 0 if a_t[0] == 1 else 1
 
 		r_store = np.append(r_store, r_t)
 		state_store = np.append(state_store, s_t, axis = 0)
 		output_store = np.append(output_store, y)
 		critic_store = np.append(critic_store, critic_reward)
-
+		
+		s_t = np.append(x_t, s_t[:, :, :, :3], axis=3)
 		print("Frame = " + str(T) + ", Updates = " + str(EPISODE) + ", Thread = " + str(thread_id) + ", Output = "+ str(intermediate_output))
 	
 	if terminal == False:
 		r_store[len(r_store)-1] = critic_store[len(r_store)-1]
 	else:
 		r_store[len(r_store)-1] = -1
-		FIRST_FRAME = True
+		s_t = np.concatenate((x_t, x_t, x_t, x_t), axis=3)
 	
 	for i in range(2,len(r_store)+1):
 		r_store[len(r_store)-i] = r_store[len(r_store)-i] + GAMMA*r_store[len(r_store)-i + 1]
 
-	return t, state_store, output_store, r_store, critic_store, FIRST_FRAME
+	return s_t, state_store, output_store, r_store, critic_store
 
-
+#function to decrease the learning rate after every epoch. In this manner, the learning rate reaches 0, by 20,000 epochs
 def step_decay(epoch):
 	decay = 3.2e-8
 	lrate = LEARNING_RATE - epoch*decay
-	lrate = math.fabs(lrate)
+	lrate = max(lrate, 0)
 	return lrate
 
 class actorthread(threading.Thread):
-	def __init__(self,thread_id, s_t, FIRST_FRAME, t):
+	def __init__(self,thread_id, s_t):
 		threading.Thread.__init__(self)
 		self.thread_id = thread_id
-		self.s_t = s_t
-		self.FIRST_FRAME = FIRST_FRAME
-		self.t = t
+		self.next_state = s_t
 
 	def run(self):
 		global episode_output
@@ -176,9 +172,8 @@ class actorthread(threading.Thread):
 		global episode_state
 
 		threadLock.acquire()
-		self.t, state_store, output_store, r_store, critic_store, self.FIRST_FRAME = runprocess(self.thread_id, self.s_t, self.FIRST_FRAME, self.t)
-		self.s_t = state_store[-1]
-		self.s_t = self.s_t.reshape(1, self.s_t.shape[0], self.s_t.shape[1], self.s_t.shape[2])
+		self.next_state, state_store, output_store, r_store, critic_store = runprocess(self.thread_id, self.next_state)
+		self.next_state = self.next_state.reshape(self.next_state.shape[1], self.next_state.shape[2], self.next_state.shape[3])
 
 		episode_r = np.append(episode_r, r_store)
 		episode_output = np.append(episode_output, output_store)
@@ -187,43 +182,45 @@ class actorthread(threading.Thread):
 
 		threadLock.release()
 
-states = np.zeros((16, IMAGE_ROWS, IMAGE_COLS, 4))
-FF = []
-tt = []
+states = np.zeros((0, IMAGE_ROWS, IMAGE_COLS, 4))
 
-for i in range(0,THREADS):
-	FF.append(True)
-	tt.append(0)
+#initializing state of each thread
+for i in range(0, len(game_state)):
+	image = game_state[i].getCurrentFrame()
+	image = preprocess(image)
+	state = np.concatenate((image, image, image, image), axis=3)
+	states = np.append(states, state, axis = 0)
 
 while True:	
 	threadLock = threading.Lock()
 	threads = []
 	for i in range(0,THREADS):
-		threads.append(actorthread(i,states[i], FF[i], tt[i]))
+		threads.append(actorthread(i,states[i]))
 
 	states = np.zeros((0, IMAGE_ROWS, IMAGE_COLS, 4))
 
 	for i in range(0,THREADS):
 		threads[i].start()
 
+	#thread.join() ensures that all threads fininsh execution before proceeding further
 	for i in range(0,THREADS):
 		threads[i].join()
 
 	for i in range(0,THREADS):
-		states = np.append(states, threads[i].s_t, axis = 0)
-		FF[i] = threads[i].FIRST_FRAME
-		tt[i] = threads[i].t
+		state = threads[i].next_state
+		state = state.reshape(1, state.shape[0], state.shape[1], state.shape[2])
+		states = np.append(states, state, axis = 0)
 
 	e_mean = np.mean(episode_r)
+	#advantage calculation for each action taken
 	advantage = episode_r - episode_critic
 	print("backpropagating")
 
 	lrate = LearningRateScheduler(step_decay)
 	callbacks_list = [lrate]
 
-	#loss = model.train_on_batch(episode_state, [episode_output, episode_critic], sample_weight = advantage)
-	weights = {'o_P':advantage, 'o_V':np.ones(len(advantage))}
-	
+	weights = {'o_P':advantage, 'o_V':np.ones(len(advantage))}	
+	#backpropagation
 	history = model.fit(episode_state, [episode_output, episode_r], epochs = EPISODE + 1, batch_size = len(episode_output), callbacks = callbacks_list, sample_weight = weights, initial_epoch = EPISODE)
 
 	episode_r = []
@@ -235,6 +232,6 @@ while True:
 	f.write("Update: " + str(EPISODE) + ", Reward_mean: " + str(e_mean) + ", Loss: " + str(history.history['loss']) + "\n")
 	f.close()
 
-	if EPISODE % 50 == 0:
-		model.save("saved_models/model_updates" + str(EPISODE))
+	if EPISODE % 50 == 0: 
+		model.save("saved_models/model_updates" +	str(EPISODE)) 
 	EPISODE += 1
