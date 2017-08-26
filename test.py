94a7ad67224d3f539b5788383000e2fb023c30b6
Shalabh Singh <shalabhsingh95@gmail.com>
2017-08-27 01:04:46 +0530

    printing score on console
    This update includes current score and top score to be printed on console. Also fps is passed to GameState constructor now

--- test.py
+++ test.py
@@ -18,50 +18,59 @@ import keras.backend as K
 BETA = 0.01
 const = 1e-5
 
-def bce(y_true, y_pred):
+#loss function for policy output
+def logloss(y_true, y_pred):     #policy loss
 	return -K.sum( K.log(y_true*y_pred + (1-y_true)*(1-y_pred) + const), axis=-1) 
-	#+ BETA * K.sum(y_pred * K.log(y_pred) + (1-y_pred) * K.log(1-y_pred))     #regularisation term
+	# BETA * K.sum(y_pred * K.log(y_pred + const) + (1-y_pred) * K.log(1-y_pred + const))   #regularisation term
 
-def ss(y_true, y_pred):
+#loss function for critic output
+def sumofsquares(y_true, y_pred):        #critic loss
 	return K.sum(K.square(y_pred - y_true), axis=-1)
 
 def preprocess(image):
 	image = skimage.color.rgb2gray(image)
-	image = skimage.transform.resize(image, (84,84), mode = 'constant')
+	image = skimage.transform.resize(image, (85,84), mode = 'constant')
 	image = skimage.exposure.rescale_intensity(image, out_range=(0,255))
 	image = image.reshape(1, image.shape[0], image.shape[1], 1)
 	return image
 
-model = load_model("good_models/model2", custom_objects={'binarycrossentropy': bce, 'sumofsquares': ss})
-#You can choose which model to run among model1, model2,... and model5.
-game_state = game.GameState()
+model = load_model("saved_models/model_updates9500", custom_objects={'logloss': logloss, 'sumofsquares': sumofsquares})
+game_state = game.GameState(30)
 
+currentScore = 0
+topScore = 0
 a_t = [1,0]
 FIRST_FRAME = True
 
-while True:	
-	x_t, r_t, terminal = game_state.frame_step(a_t)	
-	x_t = preprocess(x_t)
-
+terminal = False
+r_t = 0
+while True:
 	if FIRST_FRAME:
-		s_t = np.concatenate((x_t, x_t, x_t, x_t), axis=3)		
+		x_t = game_state.getCurrentFrame()
+		x_t = preprocess(x_t)
+		s_t = np.concatenate((x_t, x_t, x_t, x_t), axis=3)
+		FIRST_FRAME = False		
 	else:
+		x_t, r_t, terminal = game_state.frame_step(a_t)
+		x_t = preprocess(x_t)
 		s_t = np.append(x_t, s_t[:, :, :, :3], axis=3)
-
+	
 	y = model.predict(s_t)
-	no = np.random.random()
+	no = np.random.random()	
+	no = np.random.rand()
+	a_t = [0,1] if no < y[0] else [1,0]    #stochastic policy
+	#a_t = [0,1] if 0.5 <y[0] else [1,0]   #deterministic policy
 	
-	print(y)
-	if FIRST_FRAME:
-		a_t = [0,1]
-		FIRST_FRAME = False
-	else:
-		no = np.random.rand()
-		a_t = [0,1] if no < y[0] else [1,0]    #stochastic policy
-		#a_t = [0,1] if 0.5 <y[0] else [1,0]   #deterministic policy
-	
-	if r_t == -1:
+	if(r_t == 1):
+		currentScore += 1
+		topScore = max(topScore, currentScore)
+		print("Current Score: " + str(currentScore) + " Top Score: " + str(topScore))
+	if terminal == True:
 		FIRST_FRAME = True
+		terminal = False
+		currentScore = 0
+				
+
 
 #-------------- code for checking performance of saved models by finding average scores for 10 runs------------------
 
